29b3012e896aa7b06ee0000f5ca6544d48078915
Youssef BEN ZEKRI <y.benzekri@dm73.net>
2017-01-19 15:28:15 +0000

    support --custom option for selecting a different team than the favourite

--- index.js
+++ index.js
@@ -6,23 +6,37 @@ const argv = require('yargs')
     .demandOption(['game'])
     .default('game', 'next')
     .choices('game', ['next', 'previous'])
+    .option('custom', {
+        alias: 'c',
+        describe: 'custom team'
+    })
     .argv;
 const getConfig = require('./config');
-const getTeamPreviousOrNextGame = require('./api');
+const getTeamPreviousOrNextGame = require('./api').getTeamPreviousOrNextGame;
+const selectTeam = require('./selectTeam');
 
-getConfig()
-    .then(({favouriteTeam}) => {
-        return getTeamPreviousOrNextGame({teamId: favouriteTeam.id, type: argv.game})
-            .then((fixture) => {
-                process.stdout.write(`The game between ${fixture.homeTeamName} and ${fixture.awayTeamName} `);
-                if(argv.game === "previous") {
-                    process.stdout.write(` was played on ${fixture.date} and ended ${fixture.result.goalsHomeTeam}-${fixture.result.goalsAwayTeam}`);
-                } else {
-                    process.stdout.write(` will be played on ${fixture.date}`);
-                }
-            });
-    })
-    .catch((err) => {
-        console.log('Something went wrong');
-        console.log(err);
+new Promise((res, rej) => res())
+.then(() => {
+    if (argv.custom) {
+        return selectTeam();
+    } else {
+        return getConfig().then(({favouriteTeam}) => {
+            return favouriteTeam;
+        });
+    }
+})
+.then(({teamId}) => {
+    return getTeamPreviousOrNextGame({teamId: teamId, type: argv.game})
+    .then((fixture) => {
+        process.stdout.write(`The game between ${fixture.homeTeamName} and ${fixture.awayTeamName} `);
+        if(argv.game === "previous") {
+            process.stdout.write(` was played on ${fixture.date} and ended ${fixture.result.goalsHomeTeam}-${fixture.result.goalsAwayTeam} `);
+        } else {
+            process.stdout.write(` will be played on ${fixture.date}`);
+        }
+        console.log();
     });
+}).catch((err) => {
+    console.log('Something went wrong');
+    console.log(err);
+});;
